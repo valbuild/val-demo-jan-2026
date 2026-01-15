c75efcb8c1fb0b0ecf9a46a63a350e98fd13b962
Fredrik Ekholdt <fredrik.ekholdt@autodesk.com>
2026-01-15 23:13:47 +0100

    Connect project

--- val.config.ts
+++ val.config.ts
@@ -1,6 +1,9 @@
 import { initVal } from "@valbuild/next";
 
 const { s, c, val, config, nextAppRouter } = initVal({
+  project: "valbuild/val-demo-jan-2026",
+  gitBranch: process.env.VERCEL_GIT_COMMIT_REF,
+  gitCommit: process.env.VERCEL_GIT_COMMIT_SHA,
   defaultTheme: "dark",
 });
 
